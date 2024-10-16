11c968843bca502a8d3489fb739c7e1f629b17a7
upida <salmamufida34@gmail.com>
2024-10-16 21:50:26 +0700

    getFileUploadedResize: catch error

--- app/Http/Helpers/BaseFunction.php
+++ app/Http/Helpers/BaseFunction.php
@@ -6,34 +6,38 @@ trait BaseFunction
 {
     public function getFileUploadedResize($path, $size)
     {
-        $binary = file_get_contents($path);
-
-        $imageSize = getimagesizefromstring($binary);
-
-        $imageType = $imageSize['mime'];
-
-        $imageWidth = $imageSize[0];
-        $imageHeight = $imageSize[1];
-
-        if ($imageType == IMAGETYPE_JPEG) {
-            $imageOrientation = exif_read_data($path);
-            $imageOrientation = isset($imageOrientation['Orientation']) ? $imageOrientation['Orientation'] : 1;
-        } else {
-            $imageOrientation = 0;
+        try {
+            $binary = file_get_contents($path);
+    
+            $imageSize = getimagesizefromstring($binary);
+    
+            $imageType = $imageSize['mime'];
+    
+            $imageWidth = $imageSize[0];
+            $imageHeight = $imageSize[1];
+    
+            if ($imageType == IMAGETYPE_JPEG) {
+                $imageOrientation = exif_read_data($path);
+                $imageOrientation = isset($imageOrientation['Orientation']) ? $imageOrientation['Orientation'] : 1;
+            } else {
+                $imageOrientation = 0;
+            }
+    
+            // get image resize
+            $imageResize = imagecreatetruecolor($size, $size);
+            imagecopyresampled($imageResize, $imageType == IMAGETYPE_JPEG ? $imageType : imagecreatefromstring($binary), $imageOrientation == 6 || $imageOrientation == 8 ? 0 : 1, 0, 0, 0, $size, $size, $imageWidth, $imageHeight);
+    
+            // get image resize binary
+            ob_start();
+            imagejpeg($imageResize);
+            $imageResizeBinary = ob_get_contents();
+            ob_end_clean();
+    
+            $base64 = base64_encode($imageResizeBinary);
+    
+            return "data:$imageType;base64,$base64";
+        } catch (\Exception $e) {
+            return null;
         }
-
-        // get image resize
-        $imageResize = imagecreatetruecolor($size, $size);
-        imagecopyresampled($imageResize, $imageType == IMAGETYPE_JPEG ? $imageType : imagecreatefromstring($binary), $imageOrientation == 6 || $imageOrientation == 8 ? 0 : 1, 0, 0, 0, $size, $size, $imageWidth, $imageHeight);
-
-        // get image resize binary
-        ob_start();
-        imagejpeg($imageResize);
-        $imageResizeBinary = ob_get_contents();
-        ob_end_clean();
-
-        $base64 = base64_encode($imageResizeBinary);
-
-        return "data:$imageType;base64,$base64";
     }
 }
